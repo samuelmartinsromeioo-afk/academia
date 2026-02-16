914617ccb79a8daadaf1888a4d2212277cbf9c28
AlexandreSampaioGuimaraesRibeiro <lexandre.guimaraes@gmail.com>
2026-02-16 16:27:58 -0300

    layouts

--- resources/views/layouts/logins.blade.php
+++ resources/views/layouts/logins.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>@yield('title', 'RG Power Gym ')</title>
+    <title>@yield('title', 'RG Power Gym')</title>
 
     <!-- BELEZA DO SITE -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
--- resources/views/logins/index.blade.php
+++ resources/views/logins/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.index')
+@extends('layouts.logins')
 @section('title', 'Clientes')   
 @section('content')
 
